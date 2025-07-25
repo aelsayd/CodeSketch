5e0ca75d900e209cd640835c8cf68094ecf47530
Ali <aelsayedt@gmail.com>
2025-07-25 13:32:26 +0100

    feat: sideview with canvas execution

---
+++ views/canvas-view.tsx
@@ -0,0 +1,62 @@
+import { P5Runner } from '../components/p5Runner';
+import { ItemView, WorkspaceLeaf } from 'obsidian';
+import { createElement } from "react";
+import { createRoot } from 'react-dom/client';
+
+export const CANVAS_VIEW = 'canvas-view';
+
+export const Canvas = ({ code }: { code: string }) => {
+  console.log("rendering: ", code)
+
+  return (
+    <div style={{
+      height: "100%"
+    }}>
+      <h1>test</h1>
+      <P5Runner code={code} blockId={"CANVAS_VIEW_FRAME_" + Math.random()} />
+    </div>
+  )
+}
+
+export class CanvasView extends ItemView {
+  private code: string;
+
+  setCode(code: string) {
+    this.code = code;
+
+    const container = this.containerEl.children[1];
+    container.empty();
+    const root = createRoot(container);
+    root.render(
+      createElement(Canvas, { code: this.code })
+    );
+  }
+
+  constructor(leaf: WorkspaceLeaf, code: string) {
+    super(leaf);
+    this.code = code;
+  }
+
+  getViewType() {
+    return CANVAS_VIEW;
+  }
+
+  getDisplayText() {
+    return 'Canvas View';
+  }
+
+  async onOpen() {
+    const container = this.containerEl.children[1];
+    container.empty();
+    const root = createRoot(container);
+    root.render(
+      createElement(Canvas, {
+        code: this.code
+      })
+    );
+  }
+
+  async onClose() {
+    // Nothing to clean up.
+  }
+}
